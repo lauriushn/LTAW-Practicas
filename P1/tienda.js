07faf26ecd8c94d264db28fd15259cbf94f1071a
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2024-02-20 14:14:04 +0100

    tienda.js

--- P1/tienda.js
+++ P1/tienda.js
@@ -9,33 +9,24 @@ const port = 9090;  //http://127.0.0.1:9090/
 const tienda = "index.html";
 const pag_error = "error.html"
 
+const pag_404 = fs.readFileSync(pag_error);
 
 //-- Creamos el servidor
 const server = http.createServer((req, res) => {
     console.log("Petición recibida!");
 
-    //-- Construir el objeto url con la url de la solicitud
-    const myURL = new URL(req.url, 'http://' + req.headers['host']);
-    console.log("URL completa: " + myURL.href);
-    console.log("  Ruta: " + myURL.pathname);
+    let myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("La URL del recurso es: " + myURL.href);
 
     //-- Leemos el fichero index.html
     fs.readFile(tienda, (err, data) => {
         if (err) {
             console.log("Error!!")
             console.log(err.message);
+            
+            res.write(pag_404);
+            res.end();
 
-            //-- Leemos el fichero error.html
-            fs.readFile(pag_error, (err, errorData) => {
-                if (err) {
-                    console.log("Error!!")
-                    console.log(err.message);
-                }
-                else {
-                    res.write(errorData);
-                    res.end();
-                }
-            })
         }
         else {  //-- Lectura normal
             res.setHeader('Content-Type','text/html');
