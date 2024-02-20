ded6e892f44896b3bcf4dcdbcb846e8f50be62e2
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2024-02-20 14:08:04 +0100

    tienda.js

--- P1/tienda.js
+++ P1/tienda.js
@@ -5,27 +5,47 @@ const http = require('http');
 const fs = require('fs');
 
 //-- Definimos las constantes
-const port = 9090;  // http://127.0.0.1:9090/
+const port = 9090;  //http://127.0.0.1:9090/
 const tienda = "index.html";
+const pag_error = "error.html"
 
 
 //-- Creamos el servidor
 const server = http.createServer((req, res) => {
     console.log("Petición recibida!");
 
-    //-- Leemos el archivo index.html
+    //-- Construir el objeto url con la url de la solicitud
+    const myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("URL completa: " + myURL.href);
+    console.log("  Ruta: " + myURL.pathname);
+
+    //-- Leemos el fichero index.html
     fs.readFile(tienda, (err, data) => {
         if (err) {
-            console.log("Error!!");
+            console.log("Error!!")
             console.log(err.message);
 
-        } else {  //-- Lectura normal
-            res.setHeader('Content-Type', 'text/html');
+            //-- Leemos el fichero error.html
+            fs.readFile(pag_error, (err, errorData) => {
+                if (err) {
+                    console.log("Error!!")
+                    console.log(err.message);
+                }
+                else {
+                    res.write(errorData);
+                    res.end();
+                }
+            })
+        }
+        else {  //-- Lectura normal
+            res.setHeader('Content-Type','text/html');
             res.write(data);
             res.end();
         }
     });
 });
+
+
 
 //-- Activar el servidor. A la escucha de peticiones
 //-- en el puerto definido
