f0b02c04b75a91c2db45b0915d083428e06b2ef3
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2024-03-02 20:30:44 +0100

    commit

---
+++ P1/producto2.js
@@ -0,0 +1,17 @@
+// Seleccionar todas las imágenes con la clase 'nike'
+const imagenes = document.querySelectorAll('.nike');
+
+// Iterar sobre cada imagen y añadir un evento de clic
+imagenes.forEach((imagen) => {
+    imagen.addEventListener('click', () => {
+        // Remover la clase 'img-seleccionada' de todas las imágenes
+        imagenes.forEach((img) => {
+            img.classList.remove('img-seleccionada');
+            img.style.transform = 'scale(1)'; // Restablecer el tamaño de la imagen
+        });
+        // Añadir la clase 'img-seleccionada' a la imagen seleccionada
+        imagen.classList.add('img-seleccionada');
+        // Aplicar un aumento de tamaño a la imagen seleccionada
+        imagen.style.transform = 'scale(1.2)';
+    });
+});
