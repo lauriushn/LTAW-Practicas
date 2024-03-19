d6bd8d7e88196099e821daf3a0aee94804c51314
l.hernandezn.2018 <l.hernandezn.2018@alumnos.urjc.es>
2024-03-19 13:31:31 +0100

    s7: commit

---
+++ P0/sesiones-clase/s7/ejemplo2/cliente-1.js
@@ -0,0 +1,9 @@
+console.log("Ejecutando Javascript...");
+
+const display = document.getElementById("display");
+const boton_test = document.getElementById("boton_test");
+
+boton_test.onclick = ()=> {
+    display.innerHTML+="<p>Hola desde JS!</p>";
+    console.log("Botón pulsado!!")
+}
